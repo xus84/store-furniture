d9433bb2021d8c6ad93dfa9a8c04e69cc6a545ea
garsan84 <jgrsan84@gmail.com>
2021-05-04 17:28:50 +0200

    JSON file load and products showed

--- homepage.js
+++ homepage.js
@@ -4,6 +4,9 @@ const productList = document.querySelector('.product-list');
 eventListeners();
 
 function eventListeners(){
+    window.addEventListener('DOMContentLoaded', () => {
+        loadJSON();
+    });
     document.querySelector('.navbar-toggler').addEventListener('click', () => {
         document.querySelector('.navbar-collapse').classList.toggle('show-navbar');
     });
@@ -12,3 +15,31 @@ function eventListeners(){
         cartContainer.classList.toggle('show-cart-container')
     });
 }
+
+function loadJSON(){
+    fetch('furniture.json')
+    .then(response => response.json())
+    .then(data => {
+        let html = '';
+        data.forEach(product => {
+          html += `
+          <div class = "product-item">
+          <div class = "product-img">
+            <img src = "${product.imgSrc}" alt = "product image">
+            <button type = "button" class = "add-to-cart-btn">
+              <i class = "fas fa-shopping-cart"></i>Add To Cart
+            </button>
+          </div>
+
+          <div class = "product-content">
+            <h3 class = "product-name">${product.name}</h3>
+            <span class = "product-category">${product.category}</span>
+            <p class = "product-price">$${product.price}</p>
+          </div>
+        </div>
+          `;
+        });
+        productList.innerHTML = html
+;    })
+}
+
